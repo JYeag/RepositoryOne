7eb352f155878bfd546c3f861fae1ae21b1d49ed
Jeremy Yeager <Talif_Moor@yahoo.com>
2016-07-20 12:10:26 -0400

    I have no idea what I'm doing

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzzConsole
+{
+    internal class FizzBuzz
+    {
+        private int max;
+
+        public FizzBuzz(int max)
+        {
+            this.max = max;
+        }
+    }
+}
